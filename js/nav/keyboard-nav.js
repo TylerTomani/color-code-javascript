15686728636ba5cd8f590a10783ebdc4d1aad9c1
tt <tt@MacBookPro.lan>
2026-01-27 05:14:08 -0500

    main

--- js/nav/keyboard-nav.js
+++ js/nav/keyboard-nav.js
@@ -2,19 +2,28 @@
 import { letterNav } from "./letter-nav.js"
 import { sideBarNav } from "./sidebar-nav.js"
 import { getFocusZone } from "./get-focus-zone.js"
-let isLetterNavEnabled = true
+import { popupLetterNav } from "../ui/popups.js"
+let isLetterNavEnabled = false
 export function keyboardNav({e}){
     const zone = getFocusZone({ e })
     if (!zone) return
     
     if (e.key === 'x' && e.shiftKey && e.metaKey) {
         isLetterNavEnabled = !isLetterNavEnabled
+        popupLetterNav.innerText = `letter navigation : ${isLetterNavEnabled}`
+        popupLetterNav.classList.add('animate')
+        setTimeout(() => {
+            popupLetterNav.classList.remove('animate')
+        }, 1000);
         return
     }
-    if(zone === 'sideBar'){
+    if(zone === 'sideBar' && !isLetterNavEnabled){
         const isHandled = sideBarNav({e})
         if(isHandled) return
+    } else {
+        letterNav({ e })
     }
-    letterNav({e})
+    console.log(isLetterNavEnabled)
+    
        
 }
