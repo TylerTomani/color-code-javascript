21822d578597af2619d26e4c0ac67c6d5b26ab0c
tt <tt@MacBookPro.lan>
2026-02-09 14:20:08 -0500

    main

--- js/ui/change-tutorial-link.js
+++ js/ui/change-tutorial-link.js
@@ -13,28 +13,19 @@ export function changeTutorialLink(e) {
             vidHref += (vidBase.includes("?") ? "&" : "?") + `&t=${ts}s`;
         }
         tutorialLink.href = vidHref
+        if(step){
+
+            console.log(step)
+        }
+        if(e.target.tagName == 'A'){
+            const vidBase = targetLink.getAttribute("data-video");
+            const ts = targetLink.getAttribute("data-timestamp");
+            let vidHref = vidBase;
+            if (ts) {
+                vidHref += (vidBase.includes("?") ? "&" : "?") + `&t=${ts}s`;
+            }
+            tutorialLink.href = vidHref
+        }
         
     }
-    if(e.target.tagName == 'A'){
-        const vidBase = targetLink.getAttribute("data-video");
-        const ts = targetLink.getAttribute("data-timestamp");
-        let vidHref = vidBase;
-        if (ts) {
-            vidHref += (vidBase.includes("?") ? "&" : "?") + `&t=${ts}s`;
-        }
-        tutorialLink.href = vidHref
-    }
-    tutorialLink.addEventListener('click', e => {
-        console.log('here')
-        window.open('_blank', tutorialLink.href)
-        console.log(tutorialLink.href)
-    })
-    tutorialLink.addEventListener('keydown', e => {
-        console.log('here')
-        window.open('_blank',tutorialLink.href)
-        const key = e.key.toLowerCase()
-        if(key === 'enter'){
-            console.log(tutorialLink)
-        }
-    })
 }
